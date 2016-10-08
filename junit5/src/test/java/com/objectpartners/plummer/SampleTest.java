6ec545b811b396223a020f1931a6426142e3efee
Mike Plummer <mike-plummer@users.noreply.github.com>
2016-10-08 09:45:25 -0500

    Creating junit5 example project
    Adding HTML & JaCoCo report generation

---
+++ junit5/src/test/java/com/objectpartners/plummer/SampleTest.java
@@ -0,0 +1,11 @@
+package com.objectpartners.plummer;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SampleTest {
+    @Test
+    public void verifyGradleReportWorks() {
+        assertNotEquals("A", "B");
+    }
+}
