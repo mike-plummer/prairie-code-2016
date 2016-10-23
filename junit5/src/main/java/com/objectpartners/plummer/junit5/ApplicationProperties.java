6558df8fd148f1f5b8119a40021d846390063182
Mike Plummer <mike-plummer@users.noreply.github.com>
2016-10-23 17:08:33 -0500

    Fixes

--- junit5/src/main/java/com/objectpartners/plummer/junit5/ApplicationProperties.java
+++ junit5/src/main/java/com/objectpartners/plummer/junit5/ApplicationProperties.java
@@ -1,12 +1,14 @@
 package com.objectpartners.plummer.junit5;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 @Configuration
 @ConfigurationProperties("application")
 public class ApplicationProperties {
